5d58aa8373b8fb9d73e10a9589c3db188975b057
mohidmakhdoomi <mohidmakhdoomi@gmail.com>
2024-07-15 01:14:12 -0400

    changed title and landing page is now just the graph vis

--- app/page.tsx
+++ app/page.tsx
@@ -58,37 +58,41 @@ export default async function Index() {
   // console.log("page", JSON.parse(initialFetch)['links'][5])
     
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
-        </div>
-      </nav>
-
-      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
-        <Header />
-        <FocusGraph data={initialFetch} />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
+      <div className="flex-1 w-full flex flex-col gap-20 items-center">
+        <FocusGraph data={initialFetch}/>
+        <SpeedInsights/>
       </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
-      <SpeedInsights />
-    </div>
+      // <div className="flex-1 w-full flex flex-col gap-20 items-center">
+    //   <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+    //     <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+    //       <DeployButton />
+    //       {isSupabaseConnected && <AuthButton />}
+    //     </div>
+    //   </nav>
+    //
+    //   <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
+    //     <Header />
+    //     <FocusGraph data={initialFetch} />
+    //     <main className="flex-1 flex flex-col gap-6">
+    //       <h2 className="font-bold text-4xl mb-4">Next steps</h2>
+    //       {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+    //     </main>
+    //   </div>
+    //
+    //   <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+    //     <p>
+    //       Powered by{" "}
+    //       <a
+    //         href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+    //         target="_blank"
+    //         className="font-bold hover:underline"
+    //         rel="noreferrer"
+    //       >
+    //         Supabase
+    //       </a>
+    //     </p>
+    //   </footer>
+    //   <SpeedInsights />
+    // </div>
   );
 }
