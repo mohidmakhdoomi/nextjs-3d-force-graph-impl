9869df6e599f62b56a1937550689d23c3a705c99
mohidmakhdoomi <mohidmakhdoomi@gmail.com>
2024-07-20 07:28:23 -0400

    debug

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,11 +1,11 @@
 import { createClient } from '@/utils/supabase/server'
-import { test } from '@/lib/neo4j'
+// import { test } from '@/lib/neo4j'
+import TestPage from "./TestPageWrapper";
 
 export default async function Page() {
   const supabase = createClient()
-  const testing = await test()
-  console.log(testing)
   const { data: notes } = await supabase.from('notes').select()
 
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+  console.log(JSON.stringify(notes, null, 2))
+  return <TestPage /> //<pre>{JSON.stringify(notes, null, 2)}</pre>
 }
